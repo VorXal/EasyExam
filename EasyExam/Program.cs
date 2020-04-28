b7eade0600257e78beed365e71f416ec3a30a0e8
alexey <alexey32101231@gmail.com>
2020-04-28 22:08:12 +0400

    release v1

---
+++ EasyExam/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Xml.Linq;
+
+namespace EasyExam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter PATH: ");
+            string fileName = Console.ReadLine();
+            XDocument doc = XDocument.Load(fileName);
+            Console.Write("Number?");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            foreach(XElement el in doc.Element("tickets").Elements("ticket"))
+            {
+                int id = (int)el.Attribute("id");
+                if(id == choice)
+                {
+                    Console.WriteLine(el.Element("answer").Value);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
